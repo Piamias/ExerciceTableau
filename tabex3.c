54b891f332f1205e0134dd7b19a86dc99a3e00a6
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-13 20:09:24 +0100

    Exercice 3 (nombre pairs)

---
+++ tabex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tab.h"
+
+void displaytabexo3(int tab[], int tabSize);
+
+int  main()
+{
+	int tab[8] = {1,2,7,3,4,5,6,7};
+
+	displaytabexo3(tab,8);
+	return 0;
+}
