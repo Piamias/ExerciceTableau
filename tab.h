5172dbfbbe47db9bbb27610e9b6777a4174c3121
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-13 20:50:20 +0100

    New version

--- tab.h
+++ tab.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void orderTabascending(int tab[], int tabSize)
+void ordertabascending(int tab[] , int tabSize)
 {
 	int save;
 	int firstValue;
@@ -29,14 +29,109 @@ void orderTabascending(int tab[], int tabSize)
 	}
 }
 
-void displaytab(int tab[] , int tabSize)
+void ordertabdescending(int tab[] , int tabSize)
+{
+	int save;
+	int firstValue;
+	int nextValue;
+
+	firstValue = 0;
+	nextValue = 1;
+	save = 0;
+	
+	while ( firstValue < tabSize-1 && nextValue < tabSize )
+	{
+		if ( tab[firstValue] < tab[nextValue] )
+		{	
+			save = tab[firstValue];
+			tab[firstValue] = tab[nextValue];
+			tab[nextValue] = save;
+			firstValue = 0;
+			nextValue = 1;
+		}
+		else
+		{
+			firstValue++;
+			nextValue++;
+		}
+	}
+}
+
+void displaytabexo1(int tab[], int tabSize)
 {
 	int i;
 
 	i = 0;
-	while ( i < tabSize )
+	while ( i < tabSize )		
 	{
-		printf("\n%d\n",tab[i]);
-		i++;
+		if ( tab[i] < 5 )	
+		{
+			printf("%d\n",tab[i]);
+			i++;
+		}
+		else
+		{
+			i++;
+		}
 	}
 }
+
+void displaytabexo2(int tab[], int tabSize)
+{
+	int i;
+
+	i = 0;
+	while ( i < tabSize )		
+	{
+		if ( tab[i] > 5 && i == tabSize -1 )
+		{
+			printf("%d\n",tab[i]);
+			i++;
+		}
+				
+		else if ( tab[i] > 5 )	
+		{
+			printf("%d,",tab[i]);
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+}
+
+void displaytabexo3(int tab[], int tabSize)
+{
+	int i;
+	int pairNumber;
+	
+	pairNumber = 0;
+	i = 0;
+	while ( i < tabSize)
+	{
+		if ( tab[i] % 2 == 0 )
+		{
+			pairNumber++;
+					i++;
+		}
+		else
+		{
+			i++;
+		}
+	}		
+	printf ("%d\n",pairNumber);
+}
+
+void displaytab(int tab[], int tabSize)
+{
+	int i;
+
+	i = 0;
+	while ( i < tabSize )		
+	{
+			printf("%d\n",tab[i]);
+			i++;
+	}
+}
+
