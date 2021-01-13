81dd58b93429dc42e6cd3ace0069b29e0853ba0f
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-13 20:42:27 +0100

    Exercice 2 (sup a 5) fonctionne seulement pour le tableau propose

---
+++ tabex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tab.h"
+
+void displaytabexo2(int tab[], int tabSize);
+
+int main()
+{
+	int tab[8] = {1,2,7,3,4,5,6,7};
+
+	displaytabexo2(tab, 8);
+	return 0;
+}
