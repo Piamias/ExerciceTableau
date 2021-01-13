d7212e03b759db83f5adeadac7654ceb73dceb0c
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-13 19:42:49 +0100

    Exercice 1 (afficher les chiffre inf a 5)

---
+++ tabex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tab.h"
+
+void displaytabexo1(int tab[], int tabSize);
+
+int  main()
+{
+	int tab[8] = {1,2,7,3,4,5,6,7};
+	
+	displaytabexo1(tab, 8);
+	return 0;
+}
+
