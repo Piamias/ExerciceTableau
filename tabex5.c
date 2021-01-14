429127d0791f07860fd9a996e5b0e20452cd090d
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-14 10:50:43 +0100

    New version with code in .c

--- tabex5.c
+++ tabex5.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tab.h"
 
-void ordertabdescending(int tab[], int tabSize);
-void displaytab(int tab[], int tabSize);
+void displaytab(int tab[], int tabSize)
+{
+	int i;
+
+	i = 0;
+	while ( i < tabSize )		
+	{
+		printf("tab [%d] = %d\n",i ,tab[i]);
+		i++;
+	}
+}
+
+void ordertabdescending(int tab[] , int tabSize)
+{
+	int save;
+	int firstValue;
+	int nextValue;
+
+	firstValue = 0;
+	nextValue = 1;
+	
+	while ( firstValue < tabSize-1 && nextValue < tabSize )
+	{
+		if ( tab[firstValue] < tab[nextValue] )
+		{	
+			save = tab[firstValue];
+			tab[firstValue] = tab[nextValue];
+			tab[nextValue] = save;
+			firstValue = 0;
+			nextValue = 1;
+		}
+		else
+		{
+			firstValue++;
+			nextValue++;
+		}
+	}
+}
+
 
 int  main()
 {
