50037ba0ddec8bbe09e60a8dfc1208f7915f6705
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-14 09:55:13 +0100

    New with function in .c

--- tabex3.c
+++ tabex3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tab.h"
 
-void displaytabexo3(int tab[], int tabSize);
+void displaytabexo3(int tab[], int tabSize)
+{
+	int i;
+	int pairNumber;
+	
+	pairNumber = 0;
+	i = 0;
+	while ( i < tabSize)
+	{
+		if ( tab[i] % 2 == 0 )
+		{
+			pairNumber++;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}		
+	printf ("%d\n",pairNumber);
+}
 
 int  main()
 {
@@ -11,3 +30,4 @@ int  main()
 	displaytabexo3(tab,8);
 	return 0;
 }
+
